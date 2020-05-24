2643c9d263ef0b277aec0bd8ec8a0e687c8d9990
martingato <56738958+martingato@users.noreply.github.com>
2020-05-24 09:34:28 +0100

    Update movieDB.js
    updated DB movies

--- movieDB.js
+++ movieDB.js
@@ -10,7 +10,17 @@ const personalMovieDB = {
     privat: false
 };
 
+const movies = {
+    logan:''
+};
 
+movies[0] = prompt('Last film that you have seen');
+movies[1] = prompt('Last film that you have seen');
+movies[2] = prompt('How would you rate it?');
+movies[3] = prompt('How yould you rate it?');
+
+console.log(movies);
+console.log(personalMovieDB);
 
 
 
