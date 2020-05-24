236d34f5318525432d2cb3d693e79b8f30ee8773
Marsel <marsel.solonets@gmail.com>
2020-05-24 10:33:44 +0100

    correct variant
    correct code

--- movieDB.js
+++ movieDB.js
@@ -1,4 +1,4 @@
-let numberOfFilms = +prompt ('How many films heve you seen yet?');
+let numberOfFilms = +prompt ('How many films heve you seen yet?', '');
 console.log(numberOfFilms);
 
 
@@ -10,19 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const movies = {
-    logan:''
-};
+// const movies = {
+//     logan:''
+// };
 
-movies[0] = prompt('Last film that you have seen');
-movies[1] = prompt('Last film that you have seen');
-movies[2] = prompt('How would you rate it?');
-movies[3] = prompt('How yould you rate it?');
+// movies[0] = prompt('Last film that you have seen');
+// movies[1] = prompt('Last film that you have seen');
+// movies[2] = prompt('How would you rate it?');
+// movies[3] = prompt('How yould you rate it?');
 
-console.log(movies);
+// console.log(movies);
+// console.log(personalMovieDB);
+
+//
+
+const a = prompt('Last film that you have seen?', ''),
+      b = prompt('How yould you rate it from 0 to 5?', ''),
+      c = prompt('Last film that you have seen?', ''),
+      d = prompt('How yould you rate it from 0 to 5', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
 console.log(personalMovieDB);
-
-//comments
 
 
 
