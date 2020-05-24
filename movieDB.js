ee94342451b2f8b2759a973e9209b0ed546d2ea4
Marsel <marsel.solonets@gmail.com>
2020-05-24 09:27:29 +0100

    added const personalMovieDB

--- movieDB.js
+++ movieDB.js
@@ -2,6 +2,14 @@ let numberOfFilms = +prompt ('How many films heve you seen yet?');
 console.log(numberOfFilms);
 
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
 
 
 
