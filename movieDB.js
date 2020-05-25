c55f5e87d3a00e9f75c63a108e4928d2f45a6a81
Marsel <marsel.solonets@gmail.com>
2020-05-25 08:56:08 +0100

    was solved 1st problem from Part2

--- movieDB.js
+++ movieDB.js
@@ -1,6 +1,5 @@
 let numberOfFilms = +prompt ('How many films heve you seen yet?', '');
-console.log(numberOfFilms);
-
+// console.log(numberOfFilms);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,29 +9,9 @@ const personalMovieDB = {
     privat: false
 };
 
-// const movies = {
-//     logan:''
-// };
+for (i = 0; i < 2; i++) {
+    const a = prompt('Last film that you have seen?', ''),
+          b = prompt('How yould you rate it from 0 to 5?', '');
 
-// movies[0] = prompt('Last film that you have seen');
-// movies[1] = prompt('Last film that you have seen');
-// movies[2] = prompt('How would you rate it?');
-// movies[3] = prompt('How yould you rate it?');
-
-// console.log(movies);
-// console.log(personalMovieDB);
-
-//
-
-const a = prompt('Last film that you have seen?', ''),
-      b = prompt('How yould you rate it from 0 to 5?', ''),
-      c = prompt('Last film that you have seen?', ''),
-      d = prompt('How yould you rate it from 0 to 5', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
-
-
+    personalMovieDB.movies[a] = b;
+};
