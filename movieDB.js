4e6824c48ebb8c1df3b3ffee4f05a12dc3e89fd7
Marsel <marsel.solonets@gmail.com>
2020-08-27 10:03:25 +0100

    mod

--- movieDB.js
+++ movieDB.js
@@ -1,4 +1,5 @@
-let numberOfFilms = prompt ('How many films heve you seen yet?', '');
+// "use strict";
+let numberOfFilms = +prompt ('How many films heve you seen yet?', '');
 console.log(numberOfFilms);
 
 const personalMovieDB = {
@@ -9,11 +10,57 @@ const personalMovieDB = {
     privat: false
 };
 
-for (i = 0; i < 2; i++) {
+do {
+    i = 0; i < 2; i++;
     const a = prompt('Last film that you have seen?', ''),
           b = prompt('How yould you rate it from 0 to 5?', '');
 
-    personalMovieDB.movies[a] = b;
-};
+// if  {
+//   personalMovieDB.movies[a] = b;
+//   console.log('done');
+
+} while (a != null && b != null & a != '' && b != '' && a.length < 10);
+
+else {
+  alert('Please, answer the questions =)');
+  i--;
+}
+
+// for (i = 0; i < 2; i++) {
+//     const a = prompt('Last film that you have seen?', ''),
+//           b = prompt('How yould you rate it from 0 to 5?', '');
+
+//     if (a != null && b != null & a != '' && b != '' && a.length < 10) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         alert('Please, answer the questions =)');
+//         i--;
+//     }
+// };  
+
+// if (personalMovieDB.count < 10) {
+//     alert('You have seen not quite many films');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+//     alert('You are expirienced viewer')
+// } else {
+//     alert('You are movie man');
+// };
 
 console.log(personalMovieDB);
+
+
+
+
+
+
+
+    // if (a === "") {
+    //     const a = prompt('Last film that you have seen?', ''),
+    //           b = prompt('How yould you rate it from 0 to 5?', '');
+    //       };
+    //       else if (a.lenght >= 10) {
+    //           const a = prompt('The name is too long. Let\'s try again', '');
+    //                 b = prompt('How yould you rate it from 0 to 5?', '');
+
+
